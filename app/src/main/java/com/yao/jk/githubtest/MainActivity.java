16866245cbfdc6c9c8dd529b8f3229c91aaabe18
22149 <2214976538@qq.com>
2019-09-17 11:15:53 +0800

    aar

--- app/src/main/java/com/yao/jk/githubtest/MainActivity.java
+++ app/src/main/java/com/yao/jk/githubtest/MainActivity.java
@@ -2,7 +2,11 @@ package com.yao.jk.githubtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.yao.jk.testlibrary.PictureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PictureActivity.class));
+            }
+        });
     }
 }
